78288d1c657cd894b17c826e86224ece9c3e1afa
Marcin Bobowicz <marcin.bobowicz@gmail.com>
2022-12-02 11:48:58 +0100

    day2 part 1 finished

---
+++ day2a.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	// "strconv"
+	"adventofcode2022/helper"
+)
+
+func main(){
+	data := helper.ReadFileString("day2.data")
+	myPoints := 0
+	for _,line := range data{
+		x:= strings.Split(line," ")
+
+		switch x[1] {
+		case "X":
+			myPoints+=1;
+		case "Y":
+			myPoints+=2;
+		case "Z":
+			myPoints+=3;
+		}
+		
+		if x[0]=="A"{
+			switch x[1] {
+			case "X":
+				myPoints+=3;
+			case "Y":
+				myPoints+=6;
+			case "Z":
+				myPoints+=0;
+			}
+		}
+		if x[0]=="B"{
+			switch x[1] {
+			case "X":
+				myPoints+=0;
+			case "Y":
+				myPoints+=3;
+			case "Z":
+				myPoints+=6;
+			}
+		}
+		if x[0]=="C"{
+			switch x[1] {
+			case "X":
+				myPoints+=6;
+			case "Y":
+				myPoints+=0;
+			case "Z":
+				myPoints+=3;
+			}
+		}
+	}
+	fmt.Println(myPoints)
+}
