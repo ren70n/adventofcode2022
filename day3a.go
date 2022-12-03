be86df9f488028e539a503d25fb37c6ae7ed8724
Marcin Bobowicz <marcin.bobowicz@gmail.com>
2022-12-03 18:40:25 +0100

    day 3, 1st challenge done

--- day3a.go
+++ day3a.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
-	// "strings"
-	// "strconv"
+	"strings"
 	"adventofcode2022/helper"
 )
 
 func main(){
 	data := helper.ReadFileString("day3.data")
+	result := ""
+	res := 0
+
+	for _, val := range data{
+		comp1, comp2 := val[:len(val)/2], val[len(val)/2:]
+
+		for _,ch := range comp1{
+			tCompare := string(rune(ch))
+			if strings.Contains(comp2, tCompare){
+				result+=tCompare
+				if ch<='Z'{
+					res += int(ch-'A')+27
+				}else{
+					res += int(ch-'a')+1
+				}
+				break
+			}
+		}
+	}
+	fmt.Println(result, res)
 }
